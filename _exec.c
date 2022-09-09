d44de36b305435fa2d016e7817a58dce7a7e39c2
Kipyegon Rotich <danyego100@gmail.com>
2022-09-09 14:19:30 -0700

    Simple shell

---
+++ _exec.c
@@ -0,0 +1,168 @@
+#include "main.h"
+/**
+ * _exec - executes a command
+ * @args: command with arguments
+ * @env: enviroment variable
+ * @av:argumments passed via nointeractive mode
+ * @count: counter executes
+ * Description: executes a command .
+ * Return: integer
+ */
+
+void _exec(char **args, char **env, char av[], int count)
+{
+	char *path, *val_path;
+	int status, builtin = 0, exerror;
+	pid_t pid;
+	struct stat stat_tok;
+
+	if (args[0] != NULL)
+		builtin = _builtin(args, env);
+	if (builtin == 0)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			if (stat(args[0], &stat_tok) == 0)
+			{
+				/*printf("comando con ruta relativa\n");*/
+				exerror = execve(args[0], args, NULL);
+				exerror == -1 ? msg(av, count, args, 127) : exerror;
+				/*exit(127);*/
+			}
+			else
+			{
+				/*printf("A buscar ruta\n");*/
+				val_path = get_env_var("PATH", env);
+				path = get_cmd_path(val_path, args[0]);
+				exerror = execve(path, args, NULL);
+				exerror == -1 ? msg(av, count, args, 127) : exerror;
+				exit(127);
+			}
+		}
+		if (pid < 0)
+		{
+			perror("error");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			waitpid(-1, &status, 0);
+		}
+	}
+}
+
+/**
+ * get_env_var - searches for a variable on env.
+ * @variable: name of variable
+ * @env: enviroment variable
+ * Description:search for PATH variable
+ * Return: value of variable
+ */
+char *get_env_var(char *variable, char **env)
+{
+	char *varvalue, *paths;
+	int i = 0, k = 0;
+	int j = 0;
+
+	while (env[i] != NULL)
+	{
+		/*len = 0;*/
+		varvalue = env[i];
+		/*len = _strlen(varvalue);*/
+		/*found variable PATH*/
+		for (j = 0; j <= _strlen(varvalue) && (varvalue[j] == variable[j]) ; j++)
+			;
+		if (j == 4)
+		{
+			paths = malloc(sizeof(char) * _strlen(varvalue));
+			for (j = 5; j <= _strlen(varvalue) ; j++, k++)
+				*(paths + k) = *(varvalue + j);
+			paths[k] = '\0';
+			return (paths);
+			free(paths);
+		}
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * get_cmd_path - return valid path for command
+ * @varvalue: value of path variable
+ * @command: command to execute
+ * Description: return valid path to execute command
+ * Return: string with path+command
+ */
+
+char *get_cmd_path(char *varvalue, char *command)
+{
+	char *cmdPath, *paths;
+	int sizecmd;
+	struct stat stat_cmd;
+
+	paths = strtok(varvalue, ":");
+	sizecmd = _strlen(paths) + _strlen(command) + 1;
+
+	while (paths != NULL)
+	{
+		cmdPath = malloc(sizeof(char) * (sizecmd));
+		if (cmdPath == NULL)
+			perror("Path error:");
+
+		_strcat(cmdPath, paths);
+		_strcat(cmdPath, "/");
+		_strcat(cmdPath, command);
+
+		if (stat(cmdPath, &stat_cmd) == 0)
+			return (cmdPath);
+		free(cmdPath);
+		paths = strtok(NULL, ":");
+	}
+
+	return (NULL);
+}
+
+/**
+ * exitp - free command buffer.
+ * @buffer: command read
+ * Description: free buffer
+ * Return: nothing
+ */
+void exitp(char *buffer)
+{
+	if (_strcmp("exit", buffer) == 0 && buffer != NULL)
+	{
+		free(buffer);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+
+/**
+ * msg - error message
+ * @av: non interactive
+ * @count: counter
+ * @args: interactive
+ * @error: number
+ * Description: free buffer
+ * Return: 0
+ */
+int msg(char av[], int count, char **args, int error)
+{
+	/*printf("función mensajes\n");*/
+	/*printf("contador: %d",count);*/
+	write(STDOUT_FILENO, av, _strlen(av));
+write(STDOUT_FILENO, ": ", 2);
+write(STDOUT_FILENO, &count, 2);
+write(STDOUT_FILENO, ": ", 2);
+write(STDOUT_FILENO, args[0], _strlen(args[0]));
+write(STDOUT_FILENO, ": ", 2);
+/*printf("error: %d",error);*/
+if (error == 126)
+	write(STDOUT_FILENO, "Permission denied\n", 10);
+else
+	write(STDOUT_FILENO, "Not Found\n", 10);
+	exit(127);
+
+return (0);
+}
---
+++ _main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ * @argc : argc contains the number of arguments passed to the program
+ * @argv : A vector is a one-dimensional array
+ * @env : Enviroment Variable
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[], char **env)
+{
+	int status = 1;
+	char *linea;
+	char **args = NULL;
+	static int count;
+
+	count = 0;
+	signal(SIGINT, ctrc);
+
+	/*(void ) (args);*/
+	do {
+		linea = _prompt();
+		count++;
+		exitp(linea);
+		/*write(1,linea,15);*/
+		/*printf("antes de separar\n");*/
+		args = _separaArgs(linea);
+		/*printf("argc: %d\n",argc);*/
+		/*printf("argv[0]: %s\n",argv[0]);*/
+		/*printf("args = %s\n", *args);*/
+		if (argc == 1)
+		{
+			/*printf("modo interactivo\n");*/
+			_exec(args, env, argv[0], count);
+		}
+		else
+		{
+			/*printf("modo no interactivo");*/
+			_exec(argv, env, argv[0], count);
+		}
+		free(linea);
+	} while (status);
+	freedp(args);
+	exit(0);
+}
